64ae4359532644d3d7c00c106265fe1f2db173f9
JoseLomero <joselomero0@gmail.com>
2021-01-22 20:00:41 +0100

    Calendario con eventos funcional, falta mostrar horas en detalle

---
+++ src/calendar/calendar.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import 'moment/locale/es';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import events from '../backend/events'
+import Swal from 'sweetalert2'
+
+
+const localizer = momentLocalizer(moment);
+
+class Calendario extends Component {
+    constructor() {
+        super();
+        this.state = { events };
+    }
+
+    handleSelect = ({ start, end }) => {
+        Swal.fire({
+            title: 'Introduce el evento:',
+            icon: 'question',
+            input: 'text',
+            confirmButtonText: 'Aceptar',
+        }).then((result) => {
+            if (result.value) {
+                const title = JSON.stringify(result.value)
+                this.setState({
+                    events: [
+                        ...this.state.events,
+                        {
+                            start,
+                            end,
+                            title,
+                        },
+                    ],
+                })
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="calendar-container">
+                <Calendar
+                    selectable
+                    localizer={localizer}
+                    events={this.state.events}
+                    defaultView="month"
+                    defaultDate={new Date()}
+                    style={{ height: "70vh", width: "70vw" }}
+                    views={['month', 'day', 'agenda']}
+
+                    onSelectEvent={event =>
+                        Swal.fire({
+                            title: event.title,
+                            html: 'El evento empieza a las <code>${event.start}</code></br> Y termina a las <code>${event.end}</code>',
+                            confirmButtonText: 'Aceptar',
+                        })
+                        // console.log(event)
+                    }
+                    onSelectSlot={this.handleSelect}
+
+                />
+            </div>
+        );
+    }
+}
+
+export default Calendario;
